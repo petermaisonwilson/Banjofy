59d1b3061d773fd8e27c4bc30c234b704a6f8bf9
petermaisonwilson <peter@maisonwilson.fr>
2026-06-25 16:54:33 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python -m PyInstaller --noconfirm --clean --windowed --name Banjofy --collect-all PySide6 src\banjofy\__main__.py
-if errorlevel 1 pause
